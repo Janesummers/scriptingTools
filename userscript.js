8e23ef2042cc64bf706b229f5889134e84cf8ff0
Janesummer <1752321720@qq.com>
2023-10-12 17:46:10 +0800

    修改版本号

--- userscript.js
+++ userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JSummer - 草榴
 // @namespace    http://tampermonkey.net/
-// @version      0.9
+// @version      0.91
 // @description  try to take over the world!
 // @author       You
 // @match        https://*.t66y.com/*
