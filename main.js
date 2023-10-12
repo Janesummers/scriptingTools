1a3ffa063a431e64ea011803b9bcc2bc51c4a0b7
Janesummer <1752321720@qq.com>
2023-10-12 18:49:14 +0800

    ✨ feat:修改脚本

--- main.js
+++ main.js
@@ -329,11 +329,6 @@ app.post('/updateDesignationLog', (req, resp) => {
               data.read.push(item)
             }
             break;
-          case '2':
-            if (!data.unDown.includes(item)) {
-              data.unDown.push(item)
-            }
-            break;
           case '3':
             if (!data.wm.includes(item)) {
               data.wm.push(item)
@@ -365,7 +360,7 @@ app.post('/updateDesignationLog', (req, resp) => {
       }
     })
   }
-  console.log("updateDesignationLog - 准备写入", isChange, isUpdate, type, code, data);
+  console.log("updateDesignationLog - 准备写入", isChange, isUpdate, type, code);
   if (isChange) {
     const toJson = jsonToString(data, true)
     fs.writeFileSync(path.resolve(__dirname, 'merge.json'), toJson);
