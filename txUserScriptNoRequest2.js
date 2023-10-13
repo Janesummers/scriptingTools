b4a46046bb90671caada9ab7f042a2eadc462473
Janesummer <1752321720@qq.com>
2023-10-13 11:17:30 +0800

    ✨ feat:新增糖心详情页不发请求版本

---
+++ txUserScriptNoRequest2.js
@@ -0,0 +1,80 @@
+// ==UserScript==
+// @name         JSummer - 糖心 - 详情（不发请求版）
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  try to take over the world!
+// @author       You
+// @match        https://*.txh016.com/movie/detail/*
+// @match        https://txh016.com/movie/detail/*
+// @resource customCSS https://chiens.cn/recordApi/message.css
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
+// @run-at       document-end
+// @downloadURL https://chiens.cn/recordApi/txUserScriptNoRequest2.js
+// @updateURL https://chiens.cn/recordApi/txUserScriptNoRequest2.js
+// ==/UserScript==
+
+let messageScript = document.createElement('script');
+messageScript.type = 'text/javascript';
+messageScript.src = `https://chiens.cn/recordApi/message.min.js`;
+document.body.appendChild(messageScript);
+
+sessionStorage.setItem('dialogAd', 'sonofbitch');
+sessionStorage.setItem('noticeDialog', 'sonofbitch');
+sessionStorage.setItem('splashAd', 'sonofbitch');
+
+const css = GM_getResourceText("customCSS");
+GM_addStyle(css);
+    
+var style = document.createElement('style');
+document.head.appendChild(style);
+let sheet = style.sheet;
+sheet.addRule('div[checked]', 'color: #b58226 !important;');
+
+console.log('等待详情脚本执行');
+
+window.globalHint = null
+
+function recordText() {
+  let userCode = ''
+  let title = ''
+  
+  if (document.querySelector('.nav-content')) {
+    userCode = document.querySelector('.nav-content').href.match(/(\d{5,})/)
+  }
+  if (document.querySelector('.video-info .control')) {
+    userCode = document.querySelector('.video-info .control>a').href.match(/(\d{5,})/)
+  }
+  if (document.querySelector('.introduction .name')) {
+    title = document.querySelector('.introduction .name').innerText
+  }
+  if (document.querySelector('.video-info .info-actions .title')) {
+    title = document.querySelector('.video-info .info-actions .title').innerText
+  }
+  if (userCode && title !== '') {
+    userCode = userCode[0]
+    console.log('发请求')
+    window.globalHint = Qmsg.info("正在发请求", {autoClose: false});
+
+    let script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.innerHTML =
+    `
+      function handleSuccess() {
+        window.globalHint.close()
+        Qmsg.success("成功记录", {autoClose: true, onClose: () => { window.globalHint.close() }});
+      };
+    `;
+    document.body.appendChild(script);
+
+
+    let script2 = document.createElement('script');
+    script2.type = 'text/javascript';
+    script2.src = `https://chiens.cn/recordApi/txLogInScript?userCode=${userCode}&title=${title}`;
+    document.body.appendChild(script2);
+  } else {
+    setTimeout(recordText, 1000)
+  }
+}
+
+recordText()
