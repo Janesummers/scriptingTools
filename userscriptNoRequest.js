5eb2d745302e3606e8f1b27ad1850b5f0c5c6879
Janesummer <1752321720@qq.com>
2023-10-13 14:14:51 +0800

    ✨ feat:更新脚本

--- userscriptNoRequest.js
+++ userscriptNoRequest.js
@@ -17,110 +17,7 @@
 let messageScript = document.createElement('script');
 messageScript.type = 'text/javascript';
 messageScript.src = `https://chiens.cn/recordApi/message.min.js`;
-document.body.appendChild(messageScript);
-
-// messageScript.onload = function() {
-//     Qmsg.success("加载完成", {autoClose: true, onClose: () => { }});
-// }
-
-let script = document.createElement('script');
-script.type = 'text/javascript';
-script.innerHTML =
-`
-  function handleSuccess() {
-    console.log(window.globalHint, '123')
-    
-    Qmsg.success("成功记录", {autoClose: true, onClose: () => { }});
-    window.isRecord = false
-  };
-`;
-document.body.appendChild(script);
-
-const css = GM_getResourceText("customCSS");
-GM_addStyle(css);
-GM_addStyle(`
-a[checked]::before {
-  content: '[已看]';
-  font-size: 23px;
-}
-`);
-
-
-console.log('等待脚本执行');
-
-let codeList = []
-window.globalHint = null
-window.isRecord = false
-window.isLoading = false
-
-
-var style = document.createElement('style');
-document.head.appendChild(style);
-let sheet = style.sheet;
-sheet.addRule('a:visited', 'color: #b58226 !important;');
-sheet.addRule('a[checked]', 'color: #b58226 !important;');
-sheet.addRule('#main a', 'font-size: 14px;font-weight: 800;');
-sheet.addRule('#tbody .tal a[local]', 'color: #9126b5 !important;');
-sheet.addRule('#tbody .tr3 a[btn]', 'padding: 6px 14px;color: #3c3c3c !important;');
-
-function listHandle() {
-    let tbody = document.querySelector('#tbody')
-    let child = []
-    if (!tbody && location.pathname.indexOf('thread0806') === -1) {
-        tbody = document.querySelector('#main .t tbody')
-        child = tbody.querySelectorAll('.t_one')
-    }
-    if (!tbody && location.pathname.indexOf('thread0806') !== -1) {
-        // tbody = document.querySelector('#main .t tbody')
-        child = document.querySelectorAll('.t_one')
-    }
-    
-    if (child.length > 0) {
-        for (let i = 0; i < child.length; i++) {
-            let item = child[i].querySelector('.tal a[id]')
-            if (location.pathname.indexOf('thread0806') !== -1) {
-                item = child[i].querySelector('a')
-            }
-            if (!item) {
-                item = child[i].querySelector('th a')
-            }
-            if (item) {
-                item.addEventListener('click', function(){
-                    // console.log('123', this)
-                    if (this.getAttribute('checked')) {
-
-                    } else {
-                        this.setAttribute('checked','1')
-                    }
-                }, false)
-
-                if (codeList.length) {
-                    let id = item.getAttribute('id')
-                    if (id) {
-                        id = id.replace(/[^\d]*(\d+)/, "$1")
-                    } else {
-                        id = item.getAttribute('href').match(/\d{5,}/)
-                        if (id) {
-                            id = id[0]
-                        }
-                    }
-                    if (id && codeList.includes(id)) {
-                        item.setAttribute('checked','1')
-                    }
-                }
-            }
-        }
-        window.isLoading = false
-        window.globalHint = Qmsg.success("处理完成", {autoClose: true, onClose: () => {  }});
-    } else {
-        window.isLoading = false
-        window.globalHint = Qmsg.success("未获取到节点", {autoClose: true, onClose: () => {  }});
-    }
-        // console.log('item', item.getAttribute('id'))
-        
-}
-
-Promise.all([messageScript.onload, window.onload]).then(() => {
+messageScript.onload = function () {
     let tbody = document.getElementById('tbody')
 
     if (!tbody && location.pathname.indexOf('htm_mob') === -1) {
@@ -140,7 +37,7 @@ Promise.all([messageScript.onload, window.onload]).then(() => {
         if (window.isLoading) return
         window.isLoading = true
         
-        window.globalHint = Qmsg.info("正在处理文件", {autoClose: false});
+        window.globalHint = Qmsg.info("正在处理文件", {autoClose: true});
 
         let t66y = GM_getResourceText("source")
         if (t66y) {
@@ -264,12 +161,120 @@ Promise.all([messageScript.onload, window.onload]).then(() => {
     //             document.getElementById('tbody').getElementsByClassName('tal')[0].getElementsByTagName('a')[0].addEventListener('click', function(){
     //                 console.log('123', this)
     //             })
-})
+}
+document.body.appendChild(messageScript);
+
+// messageScript.onload = function() {
+//     Qmsg.success("加载完成", {autoClose: true, onClose: () => { }});
+// }
+
+let script = document.createElement('script');
+script.type = 'text/javascript';
+script.innerHTML =
+`
+  function handleSuccess() {
+    console.log(window.globalHint, '123')
+    
+    Qmsg.success("成功记录", {autoClose: true, onClose: () => { }});
+    window.isRecord = false
+  };
+`;
+document.body.appendChild(script);
+
+const css = GM_getResourceText("customCSS");
+GM_addStyle(css);
+GM_addStyle(`
+a[checked]::before {
+  content: '[已看]';
+  font-size: 23px;
+}
+`);
+
+
+console.log('等待脚本执行');
+
+let codeList = []
+window.globalHint = null
+window.isRecord = false
+window.isLoading = false
+
+
+var style = document.createElement('style');
+document.head.appendChild(style);
+let sheet = style.sheet;
+sheet.addRule('a:visited', 'color: #b58226 !important;');
+sheet.addRule('a[checked]', 'color: #b58226 !important;');
+sheet.addRule('#main a', 'font-size: 14px;font-weight: 800;');
+sheet.addRule('#tbody .tal a[local]', 'color: #9126b5 !important;');
+sheet.addRule('#tbody .tr3 a[btn]', 'padding: 6px 14px;color: #3c3c3c !important;');
+
+function listHandle() {
+    let tbody = document.querySelector('#tbody')
+    let child = []
+    if (!tbody && location.pathname.indexOf('thread0806') === -1) {
+        tbody = document.querySelector('#main .t tbody')
+        child = tbody.querySelectorAll('.t_one')
+    }
+    if (!tbody && location.pathname.indexOf('thread0806') !== -1) {
+        // tbody = document.querySelector('#main .t tbody')
+        child = document.querySelectorAll('.t_one')
+    }
+    
+    if (child.length > 0) {
+        for (let i = 0; i < child.length; i++) {
+            let item = child[i].querySelector('.tal a[id]')
+            if (location.pathname.indexOf('thread0806') !== -1) {
+                item = child[i].querySelector('a')
+            }
+            if (!item) {
+                item = child[i].querySelector('th a')
+            }
+            if (item) {
+                item.addEventListener('click', function(){
+                    // console.log('123', this)
+                    if (this.getAttribute('checked')) {
+
+                    } else {
+                        this.setAttribute('checked','1')
+                    }
+                }, false)
+
+                if (codeList.length) {
+                    let id = item.getAttribute('id')
+                    if (id) {
+                        id = id.replace(/[^\d]*(\d+)/, "$1")
+                    } else {
+                        id = item.getAttribute('href').match(/\d{5,}/)
+                        if (id) {
+                            id = id[0]
+                        }
+                    }
+                    if (id && codeList.includes(id)) {
+                        item.setAttribute('checked','1')
+                    }
+                }
+            }
+        }
+        window.isLoading = false
+        window.globalHint = Qmsg.success("处理完成", {autoClose: true, onClose: () => {  }});
+    } else {
+        window.isLoading = false
+        window.globalHint = Qmsg.success("未获取到节点", {autoClose: true, onClose: () => {  }});
+    }
+        // console.log('item', item.getAttribute('id'))
+        
+}
+
+
 
 // window.onload = function () {
     
-    
+//     return Promise.resolve()
 // }
+
+// Promise.all([messageScript.onload(), window.onload()]).then(res => {
+    
+// })
 
 // if (document.querySelector('#ajaxtable')) {
 //     document.querySelector('#ajaxtable').querySelector('.tr2 td[title="以“最後發表”顺序排列"]').style.width = '135px'
