a64c51b109034ff56bad5b203c2662685c38409e
Janesummer <1752321720@qq.com>
2023-10-13 13:11:53 +0800

    ✨ feat:更新脚本版本号

--- userscriptNoRequest.js
+++ userscriptNoRequest.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JSummer - 草榴（GET请求版）
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  try to take over the world!
 // @author       You
 // @match        https://*.t66y.com/*
