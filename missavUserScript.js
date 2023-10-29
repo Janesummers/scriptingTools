5caa8ce3faab038dd7af41dbbdf6dd3026befd49
Janesummer <1752321720@qq.com>
2023-10-29 17:56:55 +0800

    ✨ feat:新增missav脚本

---
+++ missavUserScript.js
@@ -0,0 +1,361 @@
+// ==UserScript==
+// @name         JSummer - missav
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  try to take over the world!
+// @author       You
+// @match        https://missav.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=missav.com
+// @resource customCSS https://chiens.cn/recordApi/message.css
+// @require      https://chiens.cn/recordApi/message.min.js
+// @grant        GM_xmlhttpRequest
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
+// @downloadURL https://chiens.cn/recordApi/javdbUserScript.js
+// @updateURL https://chiens.cn/recordApi/javdbUserScript.js
+// @connect      *
+// ==/UserScript==
+
+// @connect * 表示允许任何域名的跨域请求
+
+const css = GM_getResourceText("customCSS");
+GM_addStyle(css);
+GM_addStyle(`
+a:visited {
+  color: #d36141 !important;
+}
+.movie-list .box:visited .video-title {
+  color: #d36141 !important;
+}
+.movie-list .item a:visited strong {
+  color: inherit !important;
+}
+.text-sm {
+  color: #fff;
+  white-space: normal !important;
+  font-size: 18px;
+  overflow-y: visible !important;
+  max-height: none !important;
+  line-height: 24px;
+}
+.text-sm[checked='0']::before,
+.text-sm>a[checked='0']::before,
+.text-base[checked='0']::before,
+.video-number[checked='0']::before,
+.movie-panel-info .panel-block .value[checked='0']::before {
+  content: '[未录入]';
+}
+.text-sm[checked='1']::before,
+.text-sm>a[checked='1']::before,
+.text-base[checked='1']::before,
+.video-number[checked='1']::before,
+.movie-panel-info .panel-block .value[checked='1']::before {
+  content: '[已阅]';
+}
+.text-sm[checked='2']::before,
+.text-sm>a[checked='2']::before,
+.text-base[checked='2']::before,
+.video-number[checked='2']::before,
+.movie-panel-info .panel-block .value[checked='2']::before {
+  content: '[未下]';
+}
+.text-sm[checked='3']::before,
+.text-sm>a[checked='3']::before,
+.text-base[checked='3']::before,
+.video-number[checked='3']::before,
+.movie-panel-info .panel-block .value[checked='3']::before {
+  content: '[无码]';
+}
+.text-sm[checked='4']::before,
+.text-sm>a[checked='4']::before,
+.text-base[checked='4']::before,
+.video-number[checked='4']::before,
+.movie-panel-info .panel-block .value[checked='4']::before {
+  content: '[无码未下]';
+}
+.text-sm[checked='5']::before,
+.text-sm>a[checked='5']::before,
+.text-base[checked='5']::before,
+.video-number[checked='5']::before,
+.movie-panel-info .panel-block .value[checked='5']::before {
+  content: '[流出]';
+}
+.text-sm[checked='6']::before,
+.text-sm>a[checked='6']::before,
+.text-base[checked='6']::before,
+.video-number[checked='6']::before,
+.movie-panel-info .panel-block .value[checked='6']::before {
+  content: '[流出未下]';
+}
+.text-sm[checked='7']::before,
+.text-sm>a[checked='7']::before,
+.text-base[checked='7']::before,
+.video-number[checked='7']::before,
+.movie-panel-info .panel-block .value[checked='7']::before {
+  content: '[星级未下]';
+}
+.text-sm[exits='1'],
+.text-sm[exits='1']>a,
+.video-number[exits='1'] {
+  color: #b58226 !important;
+  text-decoration: underline !important;
+}
+.qmsg.qmsg-wrapper {
+  z-index: 999999;
+}
+@media (min-width: 1280px) {
+  .grid-cols-2.gap-5 {
+    grid-template-columns: repeat(2,minmax(0,1fr));
+  }
+  .mx-auto .grid-cols-2.gap-5 {
+    grid-template-columns: repeat(4,minmax(0,1fr));
+  }
+  .order-last .flex-none {
+    width: 240px !important;
+  }
+  .order-last .flex-none+div.flex-1 {
+    width: auto !important;
+  }
+  .order-last {
+    max-width: 400px !important;
+    min-width: 400px !important;
+  }
+}
+`);
+
+/*
+  0：未录入
+  1：已阅
+  2：未下
+  3：无码
+  4：无码未下
+  5：流出
+  6：流出未下
+  7：星级未下
+*/
+
+const numberExtraction = /([0-9A-Za-z][a-zA-Z0-9\_\-]+[0-9A-Za-z])|([n|k][0-9]+)/ig
+
+let isChecking = false
+let globalResult = {}
+let globalHint = null
+let isHandle = false
+
+// 判断是否是dm后面加数字的路径
+const reg = /(?<=\/)dm\d+\//
+
+const homeList = ['/', '/genres/VR']
+const skip = ['/actresses', '/genres', '/makers', '/dm15/madou', '/dm3/twav', '/dm1/furuke', '/actresses/search', '/actresses/ranking']
+const classificationList = ['/uncensored-leak', '/fc2', '/heyzo', '/tokyohot', '/1pondo', '/caribbeancom', '/caribbeancompr', '/10musume', '/pacopacomama', '/gachinco', '/xxxav', '/marriedslash', '/naughty4610', '/naughty0930', '/siro', '/luxu', '/gana', '/maan', '/scute', '/ara', '/new', '/release', '/genres/VR', '/today-hot', '/weekly-hot', '/monthly-hot', '/chinese-subtitle']
+
+function urlIncludes(url) {
+  if (url === '/') return true
+  if (homeList.includes(url)) {
+    return true
+  }
+  if (skip.includes('url')) return false
+  // return reg.test(url)
+  // let exits = false
+  // list.map(item => {
+  //   if (item !== '/' && url.indexOf(item) !== -1) {
+  //     exits = true
+  //   }
+  // })
+  // return exits
+}
+
+document.addEventListener("visibilitychange", function() {
+  var string = document.visibilityState
+  if (string === 'visible') {
+    if (urlIncludes(location.pathname)) {
+      if (isHandle) return
+      homePageListHandle()
+    }
+    if (location.pathname !== '/' && !skip.includes(location.pathname) && !urlIncludes(location.pathname)) {
+      if (isHandle) return
+      detailPageListHandle()
+    }
+    if (location.pathname === '/articles') {
+
+    }
+    if (reg.test(url)) {
+      console.log('在这');
+    }
+  }
+});
+
+window.onload = () => {
+  if (document.visibilityState === 'visible') {
+    if (urlIncludes(location.pathname)) {
+      homePageListHandle()
+    }
+    if (location.pathname !== '/' && !skip.includes(location.pathname) && !urlIncludes(location.pathname)) {
+      detailPageListHandle()
+    }
+    if (location.pathname === '/articles') {
+
+    }
+  }
+}
+
+function homePageListHandle() {
+  // let avDetailBox = document.querySelector(".movie-list")
+  // let avDetailBoxChildren = avDetailBox.children;
+  // let text = null;
+  // if (document.querySelector(".section-title")) {
+  //   text = document.querySelector(".section-title").innerText.match(numberExtraction);
+  // }
+  // text = text ? text[0] : "";
+  setTimeout(() => {
+    let list = document.querySelectorAll('.text-sm.text-nord4.truncate a[x-text], .text-sm.text-nord4.truncate a[alt]')
+    let checkList = []
+    let isDone = list[0].pathname.split('/').pop()
+    if (isDone === 'undefined') {
+      homePageListHandle()
+    } else {
+      for (let i = 0; i < list.length; i++) {
+        let code = list[i].pathname.split('/').pop().replace('-uncensored-leak', '').toUpperCase()
+        code = code.replace('FC2-PPV', 'FC2')
+        let x = code.match(numberExtraction)[0];
+        checkList.push(x)
+      }
+      checkDesignationHandle(checkList)
+    }
+  }, 1000);
+}
+
+function checkDesignationHandle(code) {
+  if (isChecking) return
+  isChecking = true
+  if (JSON.stringify(globalResult) === '{}') {
+    globalHint = Qmsg.info("正在获取数据", {autoClose: false});
+    GM_xmlhttpRequest({
+        method: "post",
+        url: "https://chiens.cn/recordApi/checkDesignationLog",
+        data: `code=${code}`,
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+
+        onload: function(req){
+          // console.log('dd', req)
+          const result = JSON.parse(req.response)
+          if (req.readyState === 4 && req.status === 200 && result.code === 'ok') {
+            const list = result.data.message
+            globalResult = list
+            console.log('globalResult', globalResult);
+            isChecking = false
+            globalHint.close()
+            globalHint = Qmsg.success("获取数据成功，等待处理", {autoClose: false, onClose: () => {  }});
+            listHandle()
+          }
+        },
+        onerror: function(response){
+          Qmsg.error("获取失败", {autoClose: true });
+          isChecking = false
+        }
+    });
+  } else {
+    globalHint = Qmsg.success("获取本地数据成功，等待处理", {autoClose: false, onClose: () => {  }});
+    listHandle()
+  }
+}
+
+function listHandle () {
+  if (urlIncludes(location.pathname)) {
+    // let avDetailBox = document.querySelector(".movie-list")
+    // let avDetailBoxChildren = avDetailBox.children;
+    let list = document.querySelectorAll('.text-sm.text-nord4.truncate a[x-text], .text-sm.text-nord4.truncate a[alt]')
+    for (let i = 0; i < list.length; i++) {
+
+      let code = list[i].pathname.split('/').pop().replace('-uncensored-leak', '').toUpperCase()
+      code = code.replace('FC2-PPV', 'FC2')
+      let x = code.match(numberExtraction)[0];
+      const text = list[i].innerText.replace('FC2-PPV', 'FC2')
+      if (text.indexOf(x) === -1) {
+        list[i].innerText = ` ${x} ${text}`
+      }
+      list[i].setAttribute('checked', globalResult[x] || '0')
+      if (globalResult[x] && globalResult[x] !== '0') {
+        list[i].setAttribute('exits', '1')
+      }
+    }
+    globalHint.close()
+    globalHint = Qmsg.success("首页列表数据 - 处理完成", {autoClose: true, onClose: () => {  }});
+  }
+  if (location.pathname !== '/' && !skip.includes(location.pathname) && !urlIncludes(location.pathname)) {
+    let list = document.querySelectorAll('div.rounded')
+    for (let i = 0; i < list.length; i++) {
+      let code = list[i].querySelector("a[alt]").pathname.split('/').pop().replace('-uncensored-leak', '').toUpperCase()
+      code = code.replace('FC2-PPV', 'FC2')
+      let x = code.match(numberExtraction)[0]
+
+      if (list[i].parentElement.querySelector('div.text-sm')) {
+        const text = list[i].parentElement.querySelector('div.text-sm a').innerText
+        if (text.indexOf(x) === -1) {
+          list[i].parentElement.querySelector('div.text-sm a').innerText =` ${x} ${text}`
+        }
+        list[i].parentElement.querySelector('div.text-sm').setAttribute('checked', globalResult[x] || '0')
+      } else {
+        const text = list[i].parentElement.parentElement.parentElement.querySelector('div.text-sm a').innerText
+        if (text.indexOf(x) === -1) {
+          list[i].parentElement.parentElement.parentElement.querySelector('div.text-sm a').innerText = ` ${x} ${text}`
+        }
+        list[i].parentElement.parentElement.parentElement.querySelector('div.text-sm').setAttribute('checked', globalResult[x] || '0')
+      }
+      if (globalResult[x] && globalResult[x] !== '0') {
+        if (list[i].parentElement.querySelector('div.text-sm')) {
+          const text = list[i].parentElement.querySelector('div.text-sm a').innerText
+          if (text.indexOf(x) === -1) {
+            list[i].parentElement.querySelector('div.text-sm a').innerText = ` ${x} ${text}`
+          }
+          list[i].parentElement.querySelector('div.text-sm').setAttribute('exits', '1')
+        } else {
+          const text = list[i].parentElement.parentElement.parentElement.querySelector('div.text-sm a').innerText
+          if (text.indexOf(x) === -1) {
+            list[i].parentElement.parentElement.parentElement.querySelector('div.text-sm a').innerText = ` ${x} ${text}`
+          }
+          list[i].parentElement.parentElement.parentElement.querySelector('div.text-sm').setAttribute('exits', '1')
+        }
+      }
+      
+    }
+
+    let t = document.querySelector('h1.text-base').innerText.match(numberExtraction);
+    t = t ? t[0] : "";
+    if (t != "") {
+      document.querySelector('h1.text-base').setAttribute('checked', globalResult[t]);
+    }
+
+    globalHint.close()
+    globalHint = Qmsg.success("详情页数据 - 处理完成", {autoClose: true, onClose: () => {  }});
+  }
+  if (location.pathname === '/articles') {
+
+  }
+  isHandle = true
+}
+
+
+// 详情页
+function detailPageListHandle() {
+  setTimeout(() => {
+    let code = location.pathname.split('/').pop().replace('-uncensored-leak', '').toUpperCase()
+    code = code.replace('FC2-PPV', 'FC2')
+    console.log('code', code);
+    let t = code.match(numberExtraction);
+    t = t ? t[0] : "";
+    let checkList = []
+    if (t !== '') {
+      checkList.push(t)
+    }
+    let list = document.querySelectorAll('div.rounded')
+    for (let i = 0; i < list.length; i++) {
+      let code = list[i].querySelector("a[alt]").pathname.split('/').pop().replace('-uncensored-leak', '').toUpperCase()
+      code = code.replace('FC2-PPV', 'FC2')
+      let x = code.match(numberExtraction)[0]
+      checkList.push(x)
+    }
+    checkDesignationHandle(checkList)
+  }, 1000);
+}
