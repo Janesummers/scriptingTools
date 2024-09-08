aa8bc57606438b1b90ae2f47137b7aa181bc900a
陈立权 <1752321720@qq.com>
2024-09-08 21:14:27 +0800

    ✨ feat:更新

--- userscript.js
+++ userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JSummer - 草榴
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://*.t66y.com/*
