5f7c6aa316f6d71654d0c48277e5f03012feb43b
陈立权 <1752321720@qq.com>
2024-09-16 20:56:02 +0800

    ✨ feat:更新

--- youtubeUserScript.js
+++ youtubeUserScript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JSummer-YouTube
 // @namespace    http://tampermonkey.net/
-// @version      1.31
+// @version      1.32
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.youtube.com/*
@@ -129,6 +129,12 @@ function record() {
           // Qmsg.success("成功记录", {autoClose: true, onClose: () => {  }});
           if (document.querySelector(".watch-active-metadata #above-the-fold #title h1.ytd-watch-metadata")) {
             document.querySelector(".watch-active-metadata #above-the-fold #title h1.ytd-watch-metadata").setAttribute('checked', '1')
+          } else {
+            setTimeout(() => {
+              if (document.querySelector(".watch-active-metadata #above-the-fold #title h1.ytd-watch-metadata")) {
+                document.querySelector(".watch-active-metadata #above-the-fold #title h1.ytd-watch-metadata").setAttribute('checked', '1')
+              }
+            }, 1000);
           }
           isRecord = false
           isChecked = true
