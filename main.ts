66d94db3e07ba9454bd55a6705e00acb925cd597
Janesummer <1752321720@qq.com>
2024-10-14 15:44:46 +0800

    ✨ feat:更新

--- main.ts
+++ main.ts
@@ -418,10 +418,15 @@ app.post('/getYoutubeLog', (_: Recordable, resp: Recordable) => {
 app.get('*', (req: Recordable, resp: Recordable) => {
   console.log(`req --> ${monent().format('YYYY-MM-DD HH:mm:ss')}`, req.params[0])
   resp.writeHead(200, { "Content-Type": "text/plain;charset=utf-8" });
-  if (/[\.js|\.css|\.json|\.out|\.pdf]$/.test(req.params[0])) {
+  if (/[\.js|\.css|\.json|\.out]$/.test(req.params[0])) {
     const file = req.params[0].replace(/\/(.*)/, '$1')
     const result = readFileFn(file)
     resp.end(result)
+  } else if (/[\.pdf]$/.test(req.params[0])) {
+    const file = req.params[0].replace(/\/(.*)/, '$1')
+    let readStream = fs.createReadStream(path.resolve(__dirname, `${file}`))
+    readStream.pipe(resp)
+    resp.end()
   } else {
     resp.status(404).end('什么也没有')
   }
