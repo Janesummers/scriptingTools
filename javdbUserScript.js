486c1434538a40f08fee9df8befb83415bad5b8e
Janesummer <1752321720@qq.com>
2024-11-15 08:55:23 +0800

    1

--- javdbUserScript.js
+++ javdbUserScript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JSummer - JavDB
 // @namespace    http://tampermonkey.net/
-// @version      2.67
+// @version      2.68
 // @description  try to take over the world!
 // @author       You
 // @match        https://javdb.com/*
