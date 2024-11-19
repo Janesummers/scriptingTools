4f57424e3cd74f078ff4c71cb69a1f8b7c2c574a
Janesummer <1752321720@qq.com>
2024-11-19 10:12:17 +0800

    1

--- javdbUserScript.js
+++ javdbUserScript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JSummer - JavDB
 // @namespace    http://tampermonkey.net/
-// @version      2.73
+// @version      2.74
 // @description  try to take over the world!
 // @author       You
 // @match        https://javdb.com/*
@@ -322,6 +322,7 @@ function checkRecordHandle() {
           }
         }
         text = textList.join('、')
+        parent.innerText = `${parent.innerText} ${text}`
         code = encodeURIComponent(location.search)
         data = `type=${type}&code=${code}&title=${text}&params=${encodeURIComponent(location.search)}`
         break;
